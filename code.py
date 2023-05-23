c71d322214e938c0afc11e35c62131bbc6458374
nethmin liyanage <nethmin.liyanage@gmail.com>
2023-05-23 16:11:38 -0700

    added for loop - not done yet -  will finish after dinner

--- code.py
+++ code.py
@@ -1,24 +1,26 @@
 import json
-with open('jsonstuff.json', 'r') as f:
-    data = json.load(f)
-#print(data)
-questions = []
-answer  = []
-for x in range(len(data['questions'])):
+for file in #insert filepath here:
+
+    with open('file', 'r') as f:
+        data = json.load(f)
+    #print(data)
+    questions = []
+    answer  = []
+    for x in range(len(data['questions'])):
     
-    questions.append(data['questions'][x]["text"])
+        questions.append(data['questions'][x]["text"])
 
-    answer.append(data['questions'][x]["answer"])
+        answer.append(data['questions'][x]["answer"])
 
-for x in range(len(questions)):
-    print(questions[x], answer[x])
+    for x in range(len(questions)):
+        print(questions[x], answer[x])
 
-promptss = ["answer this latin", "latin am i right?"]
+    promptss = ["answer this latin", "latin am i right?"]
 
-for x in range(len(questions)):
-    for prompts in promptss:
-        string = (prompts + " " + questions[x] + " " + "[ANSWER] ") 
-        newstring = 'argmax "' + string + '" from "openai/text-ada-001"'
-        finalname = str(x) + prompts + ".lmql"
-        f = open(finalname, "w")
-        f.write(newstring)
+    for x in range(len(questions)):
+        for prompts in promptss:
+            string = (prompts + " " + questions[x] + " " + "[ANSWER] ") 
+            newstring = 'argmax "' + string + '" from "openai/text-ada-001"'
+            finalname = str(x) + prompts + ".lmql"
+            f = open(finalname, "w")
+            f.write(newstring)
